f58c8c0008c9ad69df1e1d153b36887ef2f6521c
Jack <29889878+Brokenshire@users.noreply.github.com>
2019-12-18 16:51:23 +1300

    Create longest_consec.py

---
+++ Python/longest_consec.py
@@ -0,0 +1,29 @@
+import unittest
+
+
+def longest_consec(strarr, k):
+    """Returns the first longest string consisting of k consecutive strings taken in the array."""
+    if len(strarr) == 0 or k <= 0 or k > len(strarr):
+        return ""
+    lst = [''.join(strarr[i:i+k]) for i in range(len(strarr))]
+    return max(lst, key= lambda x: len(x))
+
+
+
+class TestFindShort(unittest.TestCase):
+    """Class to test longest_consec function"""
+    
+    def test_longest_string(self):
+        self.assertEqual(longest_consec(["zone", "abigail", "theta", "form", "libe", "zas"], 2), "abigailtheta")
+        self.assertEqual(longest_consec(["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"], 1), "oocccffuucccjjjkkkjyyyeehh")
+        self.assertEqual(longest_consec([], 3), "")
+        self.assertEqual(longest_consec(["itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2), "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck")
+        self.assertEqual(longest_consec(["wlwsasphmxx","owiaxujylentrklctozmymu","wpgozvxxiu"], 2), "wlwsasphmxxowiaxujylentrklctozmymu")
+        self.assertEqual(longest_consec(["zone", "abigail", "theta", "form", "libe", "zas"], -2), "")
+        self.assertEqual(longest_consec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 3), "ixoyx3452zzzzzzzzzzzz")
+        self.assertEqual(longest_consec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 15), "")
+        self.assertEqual(longest_consec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 0), "")
+
+
+if __name__ == '__main__':
+    unittest.main()
