ce1211cc6b83448d14fe6028264cf7fb19cb0b67
Jack <29889878+Brokenshire@users.noreply.github.com>
2020-04-10 20:41:12 +1200

    addding testing

---
+++ Python/__init__.py
@@ -0,0 +1,13 @@
+from 1kyu import *
+from 2kyu import *
+from 3kyu import *
+from 4kyu import *
+from 5kyu import *
+from 6kyu import *
+from 7kyu import *
+from 8kyu import *
+import unittest
+
+if __name__ == "__main__":
+
+    unittest.main()
---
+++ Python/tests.py
@@ -0,0 +1,13 @@
+from one_kyu import *
+from two_kyu import *
+from three_kyu import *
+from four_kyu import *
+from five_kyu import *
+from six_kyu import *
+from seven_kyu import *
+from eight_kyu import *
+import unittest
+
+if __name__ == "__main__":
+
+    unittest.main()
