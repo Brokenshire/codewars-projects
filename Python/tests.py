cb8fb350d4af06b050ee8ca31fdbd655f2dbc04e
Jack <29889878+Brokenshire@users.noreply.github.com>
2020-04-11 17:13:16 +1200

    Update tests.py

--- Python/tests.py
+++ Python/tests.py
@@ -1,14 +1,9 @@
-from one_kyu import *
-from two_kyu import *
-from three_kyu import *
-from four_kyu import *
-from five_kyu import *
-from six_kyu import *
-from seven_kyu import *
-from eight_kyu import *
 import unittest
 
+
+def load_tests(loader, tests, pattern):
+    return loader.discover('.')
+
+
 if __name__ == "__main__":
-    # unittest.main()
-    test_suite = unittest.TestLoader().discover('.')
-    unittest.TextTestRunner(verbosity=1).run(test_suite)
+    unittest.main()
