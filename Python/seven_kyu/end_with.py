86c75d53f31e36e057abd2e6f3f070b7b20dd785
Jack Brokenshrie <Jackb1@xtra.co.nz>
2020-04-13 17:07:31 +1200

    String ends with? solution

---
+++ Python/seven_kyu/end_with.py
@@ -0,0 +1,32 @@
+# Python solution for 'String ends with?' codewars question.
+# Level: 7 kyu
+# Tags: FUNDAMENTALS and STRINGS.
+# Author: Jack Brokenshire
+# Date: 13/04/2020
+
+import unittest
+
+
+def end_with(string, ending):
+    """
+    Boolean return value if string has the same ending as another string.
+    :param string: the string to be checked.
+    :param ending: the ending string.
+    :return: True if the last characters of string are the same as ending otherwise, False.
+    """
+    return string[len(string)-len(ending): len(string)] == ending
+
+
+class TestEndWith(unittest.TestCase):
+    """Class to test end_with function"""
+
+    def test_end_with(self):
+        self.assertEqual(end_with('abc', 'bc'), True)
+        self.assertEqual(end_with('abc', 'd'), False)
+        self.assertEqual(end_with('abcde', 'cde'), True)
+        self.assertEqual(end_with('abcde', 'abc'), False)
+        self.assertEqual(end_with('abcde', ''), True)
+
+
+if __name__ == '__main__':
+    unittest.main()
